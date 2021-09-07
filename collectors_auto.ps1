d1b3040bad7674f9446f0c564c866a40d3cabc02
native-police <53899536+native-police@users.noreply.github.com>
2021-09-07 14:23:41 +0500

    Update collectors_auto.ps1
    added sending emails

--- collectors_auto.ps1
+++ collectors_auto.ps1
@@ -9,7 +9,7 @@ foreach ($user in $users) {
     $results += New-Object psObject -Property @{'User'=$user.name;'Groups'= $groups}
     }
 
-$results | Where-Object { $_.groups -notmatch 'crm.collectorgroup' } | Select-Object user
+$results | Where-Object { $_.groups -notmatch 'crm.collectorgroup' } | Select-Object user | export-csv -Append -NoTypeInformation -force -Encoding UTF8 -Path C:\Projects_etc\crm.collector\not_collectors.csv
 
 #Костыль, ну а что поделать?
 $results2 = @()
@@ -19,4 +19,26 @@ foreach ($user in $users) {
     $groups = $user.memberof -join ';'
     $results2 += New-Object psObject -Property @{'User'=$user.name;'Groups'= $groups}
     }
-$results2 | Where-Object { $_.groups -notmatch 'crm.collectorgroup' } | Select-Object user
+$results2 | Where-Object { $_.groups -notmatch 'crm.collectorgroup' } | Select-Object user | export-csv -Append -NoTypeInformation -force -Encoding UTF8 -Path C:\Projects_etc\crm.collector\not_collectors.csv
+
+
+
+$smtpserver = "owa.2gis.ru"
+$from = "admin@chelyabinsk.2gis.ru"
+$users = Import-Csv -Path C:\Projects_etc\crm.collector\not_collectors.csv
+$encoding = [System.Text.Encoding]::UTF8
+$plaintext = @"
+Этих пользователей нет в crm.collectorgroup - оформляй заявку:
+"@
+$body_notification = Import-Csv C:\Projects_etc\crm.collector\not_collectors.csv | select user | Out-String
+
+
+
+$sendto = "admin@chelyabinsk.2gis.ru"
+
+
+Send-MailMessage -to $sendto  -Subject "crm.collectorgroup"`
+-Body $plaintext$body_notification -SmtpServer $smtpserver -From $from -Encoding $encoding
+
+
+Remove-Item "C:\Projects_etc\crm.collector\not_collectors.csv" #Удаление файла после отправки
