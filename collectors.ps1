4cb48336841dbd4f61fbbbb09f56c30c939d2667
native-police <53899536+native-police@users.noreply.github.com>
2021-09-07 12:39:28 +0500

    Add files via upload

---
+++ collectors.ps1
@@ -0,0 +1,9 @@
+﻿$results = @()
+$city=Read-Host "Please enter city you wish (RU)/Пожалуйста введите город(RU)"
+$department=Read-Host "Please enter department (RU)/Пожалуйста введите отдел(RU)"
+$users = Get-ADUser -SearchBase "OU=$department,OU=Пользователи,OU=$city,OU=Cities,DC=2gis,DC=local"  -Properties memberof -Filter * 
+foreach ($user in $users) {
+    $groups = $user.memberof -join ';'
+    $results += New-Object psObject -Property @{'User'=$user.name;'Groups'= $groups}
+    }
+$results | Where-Object { $_.groups -notmatch 'Челябинск.Access.VPN' } | Select-Object user
