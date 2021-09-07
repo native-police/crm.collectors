7182fcc838e5216ef1ea41bbb2815c4cbdf1d80b
native-police <53899536+native-police@users.noreply.github.com>
2021-09-07 12:56:50 +0500

    Add files via upload
    костыльная версия поиска по двум отделам

---
+++ collectors_auto.ps1
@@ -0,0 +1,19 @@
+﻿$results = @()
+#$city=Read-Host "Please enter city you wish (RU)/Пожалуйста введите город(RU)"
+$department="Отдел коммерции"
+$users = Get-ADUser -SearchBase "OU=$department,OU=Пользователи,OU=Челябинск,OU=Cities,DC=2gis,DC=local"  -Properties memberof -Filter * 
+foreach ($user in $users) {
+    $groups = $user.memberof -join ';'
+    $results += New-Object psObject -Property @{'User'=$user.name;'Groups'= $groups}
+    }
+$results | Where-Object { $_.groups -notmatch 'crm.collectorgroup' } | Select-Object user
+
+#Костыль, ну а что поделать?
+$results2 = @()
+$department2="Сервис менеджеры"
+$users = Get-ADUser -SearchBase "OU=$department2,OU=Пользователи,OU=Челябинск,OU=Cities,DC=2gis,DC=local"  -Properties memberof -Filter * 
+foreach ($user in $users) {
+    $groups = $user.memberof -join ';'
+    $results2 += New-Object psObject -Property @{'User'=$user.name;'Groups'= $groups}
+    }
+$results2 | Where-Object { $_.groups -notmatch 'crm.collectorgroup' } | Select-Object user
