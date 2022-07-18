134d0ad825dcdeaaa170e8a07215c054d44d2937
Denis <53899536+native-police@users.noreply.github.com>
2022-07-18 14:35:57 +0500

    Add files via upload

--- collectors_auto.ps1
+++ collectors_auto.ps1
@@ -1,44 +1,32 @@
-Import-Module ActiveDirectory
+﻿Import-Module ActiveDirectory
 
 $results = @()
-#$city=Read-Host "Please enter city you wish (RU)/Пожалуйста введите город(RU)"
-$department="Отдел коммерции"
+
+$departments = "Отдел коммерции", "Сервис менеджеры"
+foreach ($department in $departments)
+{
 $users = Get-ADUser -SearchBase "OU=$department,OU=Пользователи,OU=Челябинск,OU=Cities,DC=2gis,DC=local"  -Property memberof -Filter * 
 foreach ($user in $users) {
     $groups = $user.memberof -join ';'
-    $results += New-Object psObject -Property @{'User'=$user.name;'Groups'= $groups}
+    $results += New-Object psObject -Property @{'User'=$user.samaccountname;'Username'=$user.name;'Groups'= $groups}
     }
-
-$results | Where-Object { $_.groups -notmatch 'crm.collectorgroup' } | Select-Object user | export-csv -Append -NoTypeInformation -force -Encoding UTF8 -Path C:\Projects_etc\crm.collector\not_collectors.csv
-
-#Костыль, ну а что поделать?
-$results2 = @()
-$department2="Сервис менеджеры"
-$users = Get-ADUser -SearchBase "OU=$department2,OU=Пользователи,OU=Челябинск,OU=Cities,DC=2gis,DC=local"  -Properties memberof -Filter * 
-foreach ($user in $users) {
-    $groups = $user.memberof -join ';'
-    $results2 += New-Object psObject -Property @{'User'=$user.name;'Groups'= $groups}
-    }
-$results2 | Where-Object { $_.groups -notmatch 'crm.collectorgroup' } | Select-Object user | export-csv -Append -NoTypeInformation -force -Encoding UTF8 -Path C:\Projects_etc\crm.collector\not_collectors.csv
-
-
+$results | Where-Object { $_.groups -notmatch 'crm.collectorgroup' } | Select-Object user,name | export-csv -Append -NoTypeInformation -force -Encoding UTF8 -Path C:\Projects_etc\crm.collector\not_collectors.csv
+}
 
 $smtpserver = "owa.2gis.ru"
 $from = "admin@chelyabinsk.2gis.ru"
-$users = Import-Csv -Path C:\Projects_etc\crm.collector\not_collectors.csv
+$non_list = Import-Csv -Path C:\Projects_etc\crm.collector\not_collectors.csv
 $encoding = [System.Text.Encoding]::UTF8
 $plaintext = @"
 Этих пользователей нет в crm.collectorgroup - оформляй заявку:
 "@
-$body_notification = Import-Csv C:\Projects_etc\crm.collector\not_collectors.csv | select user | Out-String
-
-
-
+$body_notification = Import-Csv C:\Projects_etc\crm.collector\not_collectors.csv | select user,name | Out-String
 $sendto = "admin@chelyabinsk.2gis.ru"
 
 
-Send-MailMessage -to $sendto  -Subject "crm.collectorgroup"`
--Body $plaintext$body_notification -SmtpServer $smtpserver -From $from -Encoding $encoding
-
-
+if ($null -ne $non_list)
+{ Send-MailMessage -to $sendto  -Subject "crm.collectorgroup"`
+-Body $plaintext$body_notification -SmtpServer $smtpserver -From $from -Encoding $encoding }
+else {
+ }
 Remove-Item "C:\Projects_etc\crm.collector\not_collectors.csv" #Удаление файла после отправки
